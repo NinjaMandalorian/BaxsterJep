4c2775c163b0d50e8c43bee684ba3969ae03d387
Isaac <ninjamandalov@gmail.com>
2022-09-19 21:46:01 +0100

    Create HUD.java

---
+++ src/me/NinjaMandalorian/BaxsterJep/main/HUD.java
@@ -0,0 +1,23 @@
+package me.NinjaMandalorian.BaxsterJep.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class HUD {
+
+	public static int Health = 100;
+	
+	public void tick() {
+		Health = Game.clamp(Health, 0, 100);
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.gray);
+		g.fillRect(15, 15, 200, 32);
+		g.setColor(Color.green);
+		g.fillRect(15, 15, Health * 2, 32);
+		g.setColor(Color.white);
+		g.drawRect(15, 15, 200, 32);
+	}
+	
+}
